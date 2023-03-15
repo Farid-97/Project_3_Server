ea933eb36741a2d2964c64c65c4edb48cd8b7a2b
Jizzus <miguel.angelo.jesus@hotmail.com>
2023-03-15 09:09:09 +0000

    user model changed

--- models/User.model.js
+++ models/User.model.js
@@ -19,11 +19,12 @@ const userSchema = new Schema(
       required: [true, "Username is required."],
     },
     imgUrl: {
-      type:String
+      type: String,
+      default: 'https://assets.reedpopcdn.com/star-wars-is-grogu-related-to-yoda.webp/BROK/resize/1200x1200%3E/format/jpg/quality/70/star-wars-is-grogu-related-to-yoda.webp'
     },
-    post: [{ type: Schema.Types.ObjectId, ref:'Post'}],
-    favourites: [{ type: Schema.Types.ObjectId, ref:'Post'}],
-    following: [{type: Schema.Types.ObjectId, ref:'User'}]
+    post: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    favourites: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    following: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,42 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 // Get the information of the logged user
 
 router.get("/getUser", isAuthenticated, async (req, res, next) => {
-    try {
-      const user = await User.findById(req.payload._id);
-      res.json(user);
-    } catch (error) {
-      res.json(error);
-    }
-  });
-
-router.get("/getUser/:id", isAuthenticated, async (req, res, next) => {
-    const {id} = req.params
-    try {
-        const specificUser = await User.findById(id)
-        .populate('post')
-        .populate('favourites')
-        .populate({path: 'favourites',
-    populate: {path: 'comments', model: 'Comment'}})
-
-        const currentUser = await User.findById(req.payload._id);
-        res.json({specificUser, currentUser});
-    } catch (error) {
-        res.json(error);
-    }
+  try {
+    const user = await User.findById(req.payload._id).populate("post");
+    res.json(user);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
-router.put('/getUser', isAuthenticated, async(req, res, next) => {
-    try {
-        
-    } catch (error) {
-        res.json(error);
-    }
-})
+router.get("/getUser/:id", isAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const specificUser = await User.findById(id)
+      .populate("post")
+      .populate("favourites")
+      .populate({
+        path: "favourites",
+        populate: { path: "comments", model: "Comment" },
+      });
+
+    const currentUser = await User.findById(req.payload._id);
+    res.json({ specificUser, currentUser });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+router.put("/editUser", isAuthenticated, async (req, res, next) => {
+  const { username, imgUrl } = req.body;
+  try {
+   const updatedUser = await User.findByIdAndUpdate(req.payload._id, {username, imgUrl}, {new: true}); 
+   res.json(updatedUser);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 module.exports = router;
